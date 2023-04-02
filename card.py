6026f4d99c0e690e8d4acd85b11a265dfa8a6f35
aswinguvvala <85016907+aswinguvvala@users.noreply.github.com>
2023-04-01 22:26:17 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,27 +1,18 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-#pip install tensorflow
-# Imported Libraries
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-#import tensorflow as tf
+import streamlit as st
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA, TruncatedSVD
 import matplotlib.patches as mpatches
 import time
-# Classifier Libraries
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import collections
-
-
-# Other Libraries
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
@@ -33,90 +24,76 @@ from collections import Counter
 from sklearn.model_selection import KFold, StratifiedKFold
 import warnings
 warnings.filterwarnings("ignore")
-df = pd.read_csv('/content/creditcard.csv')
-df.head()
+
+# Set page title
+st.set_page_config(page_title='Credit Card Fraud Detection App')
+
+# Load data
+@st.cache(allow_output_mutation=True)
+def load_data():
+    data = pd.read_csv('creditcard.csv')
+    return data
+
+data = load_data()
+
+# Show data
+if st.checkbox('Show raw data'):
+    st.write(data)
+
+# Plot class distribution
+st.subheader('Class Distribution')
 colors = ["#0101DF", "#DF0101"]
-
-sns.countplot(x='Class', data=df, palette=colors)
+fig, ax = plt.subplots()
+sns.countplot(x='Class', data=data, palette=colors, ax=ax)
 plt.title('Class Distributions \n (0: No Fraud || 1: Fraud)', fontsize=14)
+st.pyplot(fig)
+
+# Plot amount and time distribution
+st.subheader('Amount and Time Distribution')
 fig, ax = plt.subplots(1, 2, figsize=(18,4))
-
-amount_val = df['Amount'].values
-time_val = df['Time'].values
-
+amount_val = data['Amount'].values
+time_val = data['Time'].values
 sns.distplot(amount_val, ax=ax[0], color='r')
 ax[0].set_title('Distribution of Transaction Amount', fontsize=14)
 ax[0].set_xlim([min(amount_val), max(amount_val)])
-
 sns.distplot(time_val, ax=ax[1], color='b')
 ax[1].set_title('Distribution of Transaction Time', fontsize=14)
 ax[1].set_xlim([min(time_val), max(time_val)])
+st.pyplot(fig)
 
+# Scale data
+scaler = RobustScaler()
+data['scaled_amount'] = scaler.fit_transform(data['Amount'].values.reshape(-1,1))
+data['scaled_time'] = scaler.fit_transform(data['Time'].values.reshape(-1,1))
+data.drop(['Time','Amount'], axis=1, inplace=True)
+scaled_amount = data['scaled_amount']
+scaled_time = data['scaled_time']
+data.drop(['scaled_amount', 'scaled_time'], axis=1, inplace=True)
+data.insert(0, 'scaled_amount', scaled_amount)
+data.insert(1, 'scaled_time', scaled_time)
 
-
-plt.show()
-# Since most of our data has already been scaled we should scale the columns that are left to scale (Amount and Time)
-from sklearn.preprocessing import StandardScaler, RobustScaler
-
-# RobustScaler is less prone to outliers.
-
-std_scaler = StandardScaler()
-rob_scaler = RobustScaler()
-
-df['scaled_amount'] = rob_scaler.fit_transform(df['Amount'].values.reshape(-1,1))
-df['scaled_time'] = rob_scaler.fit_transform(df['Time'].values.reshape(-1,1))
-
-df.drop(['Time','Amount'], axis=1, inplace=True)
-scaled_amount = df['scaled_amount']
-scaled_time = df['scaled_time']
-
-df.drop(['scaled_amount', 'scaled_time'], axis=1, inplace=True)
-df.insert(0, 'scaled_amount', scaled_amount)
-df.insert(1, 'scaled_time', scaled_time)
-
-# Amount and Time are Scaled!
-
-df.head()
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedShuffleSplit
-
-print('No Frauds', round(df['Class'].value_counts()[0]/len(df) * 100,2), '% of the dataset')
-print('Frauds', round(df['Class'].value_counts()[1]/len(df) * 100,2), '% of the dataset')
-
-X = df.drop('Class', axis=1)
-y = df['Class']
-
+# Split data
+X = data.drop('Class', axis=1)
+y = data['Class']
 sss = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
 
 for train_index, test_index in sss.split(X, y):
-    print("Train:", train_index, "Test:", test_index)
     original_Xtrain, original_Xtest = X.iloc[train_index], X.iloc[test_index]
     original_ytrain, original_ytest = y.iloc[train_index], y.iloc[test_index]
 
-# We already have X_train and y_train for undersample data thats why I am using original to distinguish and to not overwrite these variables.
-# original_Xtrain, original_Xtest, original_ytrain, original_ytest = train_test_split(X, y, test_size=0.2, random_state=42)
+# Check the distribution of the labels
+train_unique_label, train_counts_label = np.unique(y_train, return_counts=True)
+test_unique_label, test_counts_label = np.unique(y_test, return_counts=True)
 
-# Check the Distribution of the labels
+st.subheader("Label Distributions")
+st.write("Train set:")
+st.bar_chart(train_counts_label)
+st.write("Test set:")
+st.bar_chart(test_counts_label)
 
-
-# Turn into an array
-original_Xtrain = original_Xtrain.values
-original_Xtest = original_Xtest.values
-original_ytrain = original_ytrain.values
-original_ytest = original_ytest.values
-
-# See if both the train and test label distribution are similarly distributed
-train_unique_label, train_counts_label = np.unique(original_ytrain, return_counts=True)
-test_unique_label, test_counts_label = np.unique(original_ytest, return_counts=True)
-print('-' * 100)
-
-print('Label Distributions: \n')
-print(train_counts_label/ len(original_ytrain))
-print(test_counts_label/ len(original_ytest))
 # Since our classes are highly skewed we should make them equivalent in order to have a normal distribution of the classes.
 
 # Lets shuffle the data before creating the subsamples
-
 df = df.sample(frac=1)
 
 # amount of fraud classes 492 rows.
@@ -128,17 +105,18 @@ normal_distributed_df = pd.concat([fraud_df, non_fraud_df])
 # Shuffle dataframe rows
 new_df = normal_distributed_df.sample(frac=1, random_state=42)
 
-new_df.head()
-print('Distribution of the Classes in the subsample dataset')
-print(new_df['Class'].value_counts()/len(new_df))
+st.subheader("Distribution of Classes in Subsample Dataset")
+st.write(new_df['Class'].value_counts()/len(new_df))
 
+colors = ["#0101DF", "#DF0101"]
 
-
+st.subheader("Equally Distributed Classes")
 sns.countplot(x='Class', data=new_df, palette=colors)
 plt.title('Equally Distributed Classes', fontsize=14)
 plt.show()
-# Make sure we use the subsample in our correlation
+st.pyplot()
 
+# Make sure we use the subsample in our correlation
 f, (ax1, ax2) = plt.subplots(2, 1, figsize=(24,20))
 
 # Entire DataFrame
@@ -146,8 +124,10 @@ corr = df.corr()
 sns.heatmap(corr, cmap='coolwarm_r', annot_kws={'size':20}, ax=ax1)
 ax1.set_title("Imbalanced Correlation Matrix \n (don't use for reference)", fontsize=14)
 
-
+# Subsample DataFrame
 sub_sample_corr = new_df.corr()
 sns.heatmap(sub_sample_corr, cmap='coolwarm_r', annot_kws={'size':20}, ax=ax2)
 ax2.set_title('SubSample Correlation Matrix \n (use for reference)', fontsize=14)
-plt.show()
+
+st.subheader("Correlation Matrix")
+st.pyplot()
