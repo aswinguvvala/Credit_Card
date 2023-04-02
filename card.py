82ded4c15731fe3457d70322597438a55c1a2392
aswinguvvala <85016907+aswinguvvala@users.noreply.github.com>
2023-04-01 22:39:28 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import matplotlib
-matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA, TruncatedSVD
