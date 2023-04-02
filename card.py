00815cf001576a653238ad6031d63e66e14390f9
aswinguvvala <85016907+aswinguvvala@users.noreply.github.com>
2023-04-01 22:17:43 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,11 +1,11 @@
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in 
-pip install tensorflow
+#pip install tensorflow
 # Imported Libraries
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import tensorflow as tf
+#import tensorflow as tf
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.manifold import TSNE
